5d9b3908a566231e54a6c38521a9d956736d590f
kirti7464 <kirti227464@gmail.com>
2023-05-26 13:45:17 +0530

    added regex

--- controller/Authorcontroller/AuthorController.js
+++ controller/Authorcontroller/AuthorController.js
@@ -5,10 +5,17 @@ const createAuthor=async (req,res)=>{
      try { const data=req.body
           const{fname,lname,title,email ,password }=data
           if(!fname) return res.status(400).send("fname not found")
+          if(!fname.match(/^([...(a-z)])/gi)){
+               return res.status(400).send("error in fname")
+          }
           if(!lname) return res.status(400).send("lname not found")
           if(!title) return res.status(400).send("title not found")
           if(!email) return res.status(400).send("email not found")
+          if(!email.match(/^([...(a-z)])+([/0-9/])+@([/a-z/]+\.(com|in|org)$)/gi)){
+               return res.status(400).send("error in email")
+          }
           if(!password) return res.status(400).send("password not found")
+          data.email= email.toLowerCase()
           let c=await Author_Model.findOne({email:email})
           if(c) return res.status(400).send("email already exist")
           let create= await Author_Model.create(data)
